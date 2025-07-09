07bc1067c3426e155392f11cafdaf79a833f9537
cost-effective-digital-solution <costeffectivedigitalsolution@gmail.com>
2025-07-09 14:52:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const { generateResume } = require('./generateAI');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("PromptMyResume backend is running.");
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 
